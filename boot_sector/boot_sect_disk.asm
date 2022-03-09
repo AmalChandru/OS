78cbf1a22b71c23871ad06c798ab352797a7b775
Amal Chandran M V <amaltathva@gmail.com>
2022-03-09 19:27:01 +0530

    Update boot_sect_disk.asm

--- boot_sector/boot_sect_disk.asm
+++ boot_sector/boot_sect_disk.asm
@@ -2,7 +2,7 @@
     pusha
     
     push dx
-    mov ah, 0x02 ; ah <- int 0x13 function. 0x02 = 'read'
+    mov ah, 0x02 
     mov al, dh   ; al <- number of sectors to read (0x01 .. 0x80)
     mov cl, 0x02 ; cl <- sector (0x01 .. 0x11)
                  ; 0x01 is our boot sector, 0x02 is the first 'available' sector
